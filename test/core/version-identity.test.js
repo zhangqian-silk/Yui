ae881b0fb74837a95ee531494c482fca0bd8dbeb
SILK <64468355+zhangqian-silk@users.noreply.github.com>
2026-08-13 07:10:20 +0800

    Release Yui 0.5.0

--- test/core/version-identity.test.js
+++ test/core/version-identity.test.js
@@ -8,9 +8,9 @@ import {
 } from "../../dist/version.js";
 
 test("Yui exposes one release identity for CLI, Controller and storage diagnostics", () => {
-  assert.equal(YUI_VERSION, "0.4.2");
+  assert.equal(YUI_VERSION, "0.5.0");
   assert.deepEqual(yuiVersionIdentity(), {
-    version: "0.4.2",
+    version: "0.5.0",
     controllerProtocolVersion: 3,
     storageLayoutVersion: 6,
     aggregateSchemaVersion: 18
